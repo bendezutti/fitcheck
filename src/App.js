ccaa8fef166492f4e7d8c23624b8e8c0c1fec480
bendezutti <benjamindezutti@gmail.com>
2024-06-22 17:03:33 -0400

    initial setup of routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Router, Switch, Redirect} from 'react-router-dom'
+import Homepage from './HomePage/page/Homepage'
+import Login from './Login/page/Login'
+import Register from './Register/page/Register'
 
 function App() {
   return (
     <div>
       <Switch>
       <Router>
-        <Route> </Route>
-
+        <Route path='/home'> <Homepage/> </Route>
+        <Route path='/login'> <Login/> </Route>
+        <Route path='/register'> <Login/> <Register/> </Route>
       </Router>
       <Redirect to='/'/>
       </Switch>
